785391b1550d487c116322918e5f6b8204b23f2e
Aditya <adityaspmahajan@gmail.com>
2022-01-02 00:22:26 +0530

    Dashboard 1

--- ModernDash/app/src/main/java/com/example/moderndash/MainActivity.java
+++ ModernDash/app/src/main/java/com/example/moderndash/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.moderndash;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void dash1(View view){
+        Intent intent = new Intent(this, DashActivity1.class);
+        startActivity(intent);
+    }
+
+    public void dash2(View view){
+        Intent intent = new Intent(this, DashActivity2.class);
+        startActivity(intent);
+    }
+
+    public void dash3(View view){
+        Intent intent = new Intent(this, DashActivity3.class);
+        startActivity(intent);
     }
 }
