5f53ad0c116249424af80cd37a194fb9a707942a
Aditya <adityaspmahajan@gmail.com>
2022-01-06 22:15:49 +0530

    Dashboard 2

--- ModernDash/app/src/main/java/com/example/moderndash/DashActivity2.java
+++ ModernDash/app/src/main/java/com/example/moderndash/DashActivity2.java
@@ -3,12 +3,15 @@ package com.example.moderndash;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
 
 public class DashActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
+        getSupportActionBar().hide(); // hide the title bar
         setContentView(R.layout.activity_dash2);
     }
 }
