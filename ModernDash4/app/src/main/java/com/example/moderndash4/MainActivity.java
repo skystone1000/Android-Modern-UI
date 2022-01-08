faf4b8f4c562347351d9e844ccdedcd85ad89024
Aditya <adityaspmahajan@gmail.com>
2022-01-08 23:23:39 +0530

    Dashboard 4

--- ModernDash4/app/src/main/java/com/example/moderndash4/MainActivity.java
+++ ModernDash4/app/src/main/java/com/example/moderndash4/MainActivity.java
@@ -2,13 +2,18 @@ package com.example.moderndash4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(1);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
         setContentView(R.layout.activity_main);
     }
 }
